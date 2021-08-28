36e815664f3e18b252d56d08b774a8addad9e1b2
gaurav0116 <dhandhukiya218@gmail.com>
2021-08-28 11:23:21 +0530

    added remote mysql connection

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php
+        // Development config
+	// $servername = "localhost";
+        // $username = "root";
+        // $password = "";
+        // $database = "spark_bank_db";
 
-	$servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "spark_bank_db";
+        // Remote hosting config
+        $servername = "remotemysql.com";
+        $username = "mzQbGODaKU";
+        $password = "BDnxNO57Jg";
+        $database = "mzQbGODaKU";
 
 // Create connection
         $conn = mysqli_connect($servername, $username, $password, $database);
